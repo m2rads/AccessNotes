e8ce8a45868cd495dfb73a1468ccfba63cf5a5d8
m2rads <argo.mohrad@gmail.com>
2023-06-27 23:11:04 -0700

    add yarn build

--- src/StickyNote/StickyNote.js
+++ src/StickyNote/StickyNote.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Draggable from "react-draggable";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { updated } from "../features/noteTxt/noteTxt-slice";
 import "./StickyNote.css";
@@ -9,33 +8,31 @@ function StickyNote(props) {
   const dispatch = useAppDispatch();
 
   return (
-    <Draggable>
-      <div>
-        <div className="note-container" style={props.stickyNoteStyle}>
-          <div className="note-header">
-            <button className="close-butt" onClick={() => props.onCloseNote()}>
-              X
-            </button>
-          </div>
-          <textarea
-            className="note-content"
-            id="noteTextArea"
-            value={note || ""}
-            onChange={(e) => dispatch(updated(e.target.value))}
-            type="text"
-          />
+    <div>
+      <div className="note-container" style={props.stickyNoteStyle}>
+        <div className="note-header">
+          <button className="close-butt" onClick={() => props.onCloseNote()}>
+            X
+          </button>
+        </div>
+        <textarea
+          className="note-content"
+          id="noteTextArea"
+          value={note || ""}
+          onChange={(e) => dispatch(updated(e.target.value))}
+          type="text"
+        />
 
-          <div className="note-footer">
-            <button className="save-butt" onClick={(e) => props.onSave(note)}>
-              Save
-            </button>
-            <button className="del-butt" onClick={(e) => props.onDelete()}>
-              Delete
-            </button>
-          </div>
+        <div className="note-footer">
+          <button className="save-butt" onClick={(e) => props.onSave(note)}>
+            Save
+          </button>
+          <button className="del-butt" onClick={(e) => props.onDelete()}>
+            Delete
+          </button>
         </div>
       </div>
-    </Draggable>
+    </div>
   );
 }
 
