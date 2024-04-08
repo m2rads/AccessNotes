ed80ae24013eb525f9d4eef46791a90931118a6d
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-08 12:50:57 -0700

    delete v1.0 code

--- src/app/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import noteReducer from "../features/noteTxt/noteTxt-slice";
-
-export const store = configureStore({
-  reducer: { note: noteReducer },
-});
