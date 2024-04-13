25904e3f9be70881a2715a4ad2a2f61e21e3e041
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-13 11:18:41 -0700

    add background worker and side panel instead of popup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import StickyNotes from "./StickyNotes/StickyNotes";
 import { PopupComponent } from "./Popup/PopupComponent";
 import "./App.css"
@@ -30,4 +30,4 @@ popupRoot && // to suppress minified react error 200
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
