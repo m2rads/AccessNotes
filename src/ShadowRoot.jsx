d932b8962ea92a2dfe8e673b85b337fd5b29957b
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-16 15:59:36 -0700

    modified

--- src/ShadowRoot.jsx
+++ src/ShadowRoot.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import root from "react-shadow";
-import { css } from "@emotion/react";
 
 export const ShadowRoot = ({ children }) => {
   const [stylesNode, setStylesNode] = useState(null);
