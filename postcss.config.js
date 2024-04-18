49423232c56e10aa013ceffbe3fe627329654c41
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-18 14:28:01 -0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
   },
-}
+})
