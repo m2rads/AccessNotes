a295fb77dc1923d313f5c2cef2ad0901d2d97753
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-18 14:34:12 -0700

    add postcss and add styled components example

--- src/Tooltip/Tooltip.jsx
+++ src/Tooltip/Tooltip.jsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  z-index: 2;
+  border: 1px solid grey;
+  position: absolute;
+  background: white;
+  top: ${(props) => props.y + "px"};
+  left: ${(props) => props.x + "px"};
+`;
+
 
 export default function Tooltip() {
       
     return(
-        <div className='text-center text-xl text-gray-700 bg-gray-200 p-4'>
+        <Container>
         Hello world!
-        </div>
+        </Container>
     ) 
 }
