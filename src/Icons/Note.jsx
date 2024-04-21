9b405c8a893c1dd8db101bfffb613e8ad0335387
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-21 15:58:19 -0700

    change the color

--- src/Icons/Note.jsx
+++ src/Icons/Note.jsx
@@ -1,8 +1,8 @@
 export default function NoteIcon () {
     return(
         <svg width="24" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8.00001 14.6667C9.47277 14.6667 10.6667 13.4728 10.6667 12C10.6667 10.5272 9.47277 9.33333 8.00001 9.33333C6.52725 9.33333 5.33334 10.5272 5.33334 12C5.33334 13.4728 6.52725 14.6667 8.00001 14.6667Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M10.6667 12V1.33333" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.00001 14.6667C9.47277 14.6667 10.6667 13.4728 10.6667 12C10.6667 10.5272 9.47277 9.33333 8.00001 9.33333C6.52725 9.33333 5.33334 10.5272 5.33334 12C5.33334 13.4728 6.52725 14.6667 8.00001 14.6667Z" stroke="#f8fafc" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M10.6667 12V1.33333" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
 
     )
