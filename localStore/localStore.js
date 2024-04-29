c4d441f9f79f64266f945a9256800ac97434df3d
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-04-28 20:27:19 -0700

    add removed functionalityes

--- localStore/localStore.js
+++ localStore/localStore.js
@@ -1,10 +1,11 @@
 class LocalStore {
     constructor(id) {
         this.key = id !== undefined ? `accessnotes-${id}` : 'accessnotes';
+        this.notesKey = `notes-${id}`; // Separate key for notes
     }
 
-    storeToJson() {
-        const store = localStorage.getItem(this.key);
+    storeToJson(key = this.key) {
+        const store = localStorage.getItem(key);
         let sources;
         try {
             sources = JSON.parse(store) || [];
@@ -15,18 +16,14 @@ class LocalStore {
         return sources;
     }
 
-    jsonToStore(stores) {
-        localStorage.setItem(this.key, JSON.stringify(stores));
+    jsonToStore(stores, key = this.key) {
+        localStorage.setItem(key, JSON.stringify(stores));
     }
 
     save(data, color, tooltipPos, tooltipLoc) {  
         const stores = this.storeToJson();
         const map = {};
         stores.forEach((store, idx) => map[store.hs.id] = idx);
-
-        if (!Array.isArray(data)) {
-            data = [data];
-        }
 
         data.forEach(store => {
             store.color = color; 
@@ -41,37 +38,57 @@ class LocalStore {
         this.jsonToStore(stores);
     }
 
+    remove(id) {
+        const stores = this.storeToJson();
+        const index = stores.findIndex(store => store.hs.id === id);
+        if (index !== -1) {
+            stores.splice(index, 1);
+            this.jsonToStore(stores);
+        }
+    }
+
+    get(id) {
+        const stores = this.storeToJson();
+        return stores.find(store => store.hs.id === id) || null;
+    }
+
+    getAll() {
+        return this.storeToJson();
+    }
+
+    removeAll() {
+        this.jsonToStore([]);
+    }
+
     // Note management functions
     saveNote(id, content) {
-        const notes = this.getAllNotes();
-        const noteIndex = notes.findIndex(note => note.id === id);
-
-        if (noteIndex !== -1) {
-            notes[noteIndex].content = content;
+        const notes = this.storeToJson(this.notesKey);
+        const index = notes.findIndex(note => note.id === id);
+        if (index !== -1) {
+            notes[index].content = content;
         } else {
             notes.push({ id, content });
         }
-
-        this.jsonToStore(notes);
+        this.jsonToStore(notes, this.notesKey);
     }
 
     getNoteById(id) {
-        const notes = this.getAllNotes();
+        const notes = this.storeToJson(this.notesKey);
         return notes.find(note => note.id === id) || null;
     }
 
     removeNoteById(id) {
-        const notes = this.getAllNotes();
+        const notes = this.storeToJson(this.notesKey);
         const filteredNotes = notes.filter(note => note.id !== id);
-        this.jsonToStore(filteredNotes);
+        this.jsonToStore(filteredNotes, this.notesKey);
     }
 
     removeAllNotes() {
-        this.jsonToStore([]);
+        this.jsonToStore([], this.notesKey);
     }
 
     getAllNotes() {
-        return this.storeToJson();
+        return this.storeToJson(this.notesKey);
     }
 }
 
