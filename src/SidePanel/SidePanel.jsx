5231cacff0b3b1463ece31927771da36b8738090
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-05-02 23:27:47 -0700

    add design to side panel

--- src/SidePanel/SidePanel.jsx
+++ src/SidePanel/SidePanel.jsx
@@ -3,9 +3,8 @@ import { Folder } from "./Folder";
 export function SidePanel() {
 
     return (
-        <>
-        <p>Folders</p>
-        <Folder />
-        </>
+        <div>
+            <Folder />
+        </div>
     )
 }
