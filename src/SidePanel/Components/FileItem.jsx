73c0c8555473723938a83caab95f0fd9e90960cf
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-07-23 11:01:13 -0700

    add file item to inclcude icon down

--- src/SidePanel/Components/FileItem.jsx
+++ src/SidePanel/Components/FileItem.jsx
@@ -1,28 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FileIcon } from '../../Icons/FileIcon';
+import { ArrowRightIcon } from '../../Icons/ArrowRightIcon';
 
-const FileItem = ({ url, title, onClick }) => {
-    const [isHovered, setIsHovered] = useState(false);
-  
-    return (
-      <div 
-        className={'file-item'}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        onClick={onClick}
-      >
-        <div className="file-icon">
-          <FileIcon />
-        </div>
-        <div
-            className={`file-item-title ${isHovered ? 'label-emphasised' : 'label'}`}
-        >
-          {title}
-        </div>
+const FileItem = ({ url, title, onClick, onDragStart, onDragOver, onDrop, hasChildren }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className={'file-item'}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={onClick}
+      draggable
+      onDragStart={onDragStart}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+    >
+      <div className="file-icon">
+        {hasChildren && isHovered ? <ArrowRightIcon /> : <FileIcon />}
       </div>
-    );
+      <div
+        className={`file-item-title ${isHovered ? 'label-emphasised' : 'label'}`}
+      >
+        {title}
+      </div>
+    </div>
+  );
 };
 
-
 export default FileItem;
-  
